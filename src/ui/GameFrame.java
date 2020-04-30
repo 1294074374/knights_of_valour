ebe4c6f6879b23c7d2d76a122bd628f912929115
laijunlin <1294074374@qq.com>
2020-04-30 16:13:24 +0800

    三国战纪

---
+++ src/ui/GameFrame.java
@@ -0,0 +1,18 @@
+package ui;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+	
+	public GameFrame(){
+		// 标题
+		setTitle("雪中悍刀行");
+		// 大小
+		setSize(800,480);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
